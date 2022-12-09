05ca11c4772a23de76c3b0aa52bd4bab70a70d4a
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2022-12-09 00:38:42 -0300

    add bg images about and contact

--- src/pages/desktop/About/index.jsx
+++ src/pages/desktop/About/index.jsx
@@ -1,10 +1,13 @@
 import './style.css'
-import React from "react"
+import React from 'react'
+import BG from '../../../assets/images/desktop/AboutBG.svg'
 
 export function AboutDesk() {
   return (
     <>
-      <h1>Sobre</h1>
+      <div id="bg-color">
+        <img id="background" src={BG} alt="background" />
+      </div>
     </>
   )
 }
