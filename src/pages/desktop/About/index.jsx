3774e0138862fd839bc6e9d7ac4e411e294485f8
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2022-12-10 18:24:12 -0300

    Desktop ok!

--- src/pages/desktop/About/index.jsx
+++ src/pages/desktop/About/index.jsx
@@ -1,28 +1,27 @@
 import './style.css'
-import React from 'react'
-import BG from '../../../assets/images/desktop/AboutBG.svg'
+import ABG from '../../../assets/images/desktop/AboutBG.svg'
 
 export function AboutDesk() {
   return (
     <>
       <div id="about-main">
-        <div id="line1"> </div>
-        <div id="line2"> </div>
-        <img id="background" src={BG} alt="background" />
+        <div id="line1-a"> </div>
+        <div id="line2-a"> </div>
+        <img id="background-about" src={ABG} alt="background" />
         <div id="text-limit">
           <div id="text-about">
             <strong id="title-about">
               {' '}
               SOBRE O PROJETO <br /><br />
             </strong>{' '}
-            O projeto Pokédex foi desenvolvido utilizando o Figma para criar o
-            frontend do projeto, a API PokeAPI que faz a busca dos dados dos
-            Pokémon e as tecnologias: HTML, CSS e ReactJS. O projeto foi
-            realizado com o intuito de aprender mais sobre o consumo de APIs e ReactJS.
+            O projeto Pokédex foi desenvolvido utilizando o Figma para modelar o
+            frontend do projeto, a API PokeAPI para fazer a busca dos dados dos
+            Pokémon e as tecnologias: HTML, CSS, ReactJS e GIT. O projeto foi
+            realizado com o intuito de aprender mais sobre ReactJS e o consumo de APIs.
           </div>
         </div>
-        <div id="line3"> </div>
-        <div id="line4"> </div>
+        <div id="line3-a"> </div>
+        <div id="line4-a"> </div>
       </div>
     </>
   )
